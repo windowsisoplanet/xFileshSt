c58908c72f721c7561b832c53319d990050a039c
windowsisoplanet <105793877+windowsisoplanet@users.noreply.github.com>
2022-05-20 22:16:55 +0100

    Update and rename 28selects_for_5.js to 29selects_for_5.js

--- 29selects_for_5.js
+++ 29selects_for_5.js
@@ -210,7 +210,7 @@ var version_id = $('#version_id :selected').val();
 	$(html).find("a").each( function(){
 
     var href = $(this).attr('href');
-    $('#Dalink').html('<a href="'+href+'"></a>'); 
+    $('#infodowns').html('<a href="'+href+'"></a>'); 
 
 } )
 } );

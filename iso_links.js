e80fc40ac60ffa77b4b8359c2814996faa059b0e
windowsisoplanet <105793877+windowsisoplanet@users.noreply.github.com>
2022-05-21 11:57:57 +0100

    Add files via upload

---
+++ iso_links.js
@@ -0,0 +1,164 @@
+function Windows_11_64_english() {
+window.location.href = "https://archive.org/download/win-11-english-x-64v-1_202112/Win11_English_x64v1.iso";
+}
+function Windows_11_64_french() {
+window.location.href = "https://archive.org/download/win-11-french-x-64_202111/Win11_French_x64.iso";
+}
+function Windows_11_64_Dutch() {
+window.location.href = "https://archive.org/download/win-11-french-x-64_202111/Win11_French_x64.iso";
+//Windows 10 links ------------------------------------------------------
+}
+function Windows_10_32_english() {
+window.location.href = "https://archive.org/download/windows-10-build-21301.1000-x86-english/21301.1000.210123-1645.RS_PRERELEASE_CLIENTMULTI_X86FRE_EN-US.ISO";
+}
+function Windows_10_64_english() {
+window.location.href = "https://archive.org/download/windows10mx-blind/Win10_20H2_v2_English_x64.iso";
+}
+function Windows_10_32_french() {
+window.location.href = "https://ia804506.us.archive.org/4/items/win-10-21-h-1-french-x-32/Win10_21H1_French_x32.iso";
+}
+function Windows_10_64_french() {
+window.location.href = "https://ia804508.us.archive.org/19/items/win-10-21-h-1-french-x-64/Win10_21H1_French_x64.iso";
+}
+function Windows_10_32_Dutch() {
+window.location.href = "https://archive.org/download/windows-10-Dutch-20h2/Win10_20H2_v2_Dutch_x32.iso";
+}
+function Windows_10_64_Dutch() {
+window.location.href = "https://archive.org/download/windows-10-Dutch-20h2/Win10_20H2_v2_Dutch_x64.iso";
+}
+//Windows 8 links ------------------------------------------------------
+function Windows_8_32_english() {
+window.location.href = "https://archive.org/download/en_windows_8_x86_dvd_915417/en_windows_8_x86_dvd_915417.iso";
+}
+function Windows_8_64_english() {
+window.location.href = "https://archive.org/download/en_windows_8_x64_dvd_915440_202107/en_windows_8_x64_dvd_915440.iso";
+}
+function Windows_8_32_french() {
+window.location.href = "https://archive.org/download/Windows8Enterprise32BitFrench/fr_windows_8_enterprise_x86_dvd_917671.iso";
+}
+function Windows_8_64_french() {
+window.location.href = "https://archive.org/download/windows-8-pro_202107/Windows%208%20Pro.iso";
+}
+function Windows_8_32_Dutch() {
+window.location.href = "https://https://archive.org/download/Win8.1AIODEU/Microsoft%20Windows%208.1%20x86%20x64%20Aio%20Dutch%20%5BDDF8C503%5D.iso";
+}
+function Windows_8_64_Dutch() {
+window.location.href = "https://archive.org/download/Windows8CoreAndProDutchx64/de_windows_8_x64_dvd_915409.iso";
+}
+//Windows 7 links ------------------------------------------------------
+function Windows_7_32_english() {
+window.location.href = "https://archive.org/download/win-7-ult-32-bit-iso/GRMCULFREO_EN_DVD.ISO";
+}
+function Windows_7_64_english() {
+window.location.href = "https://archive.org/download/win-7-ult-sp-1-english-x-64/Win7_Ult_SP1_English_x64.iso";
+}
+function Windows_7_32_french() {
+window.location.href = "https://archive.org/download/fr_windows_7_professional_with_sp1_x64_x86_dvd_u/fr_windows_7_professional_with_sp1_x86_dvd_u_677092.iso";
+}
+function Windows_7_64_french() {
+window.location.href = "https://archive.org/download/fr_windows_7_professional_with_sp1_x64_x86_dvd_u/fr_windows_7_professional_with_sp1_x64_dvd_u_678724.iso";
+}
+function Windows_7_32_Dutch() {
+window.location.href = "https://archive.org/download/Windows7UltimateSP1x86Dutch/de_windows_7_ultimate_with_sp1_x86_dvd_u_677456.iso";
+}
+function Windows_7_64_Dutch() {
+window.location.href = "https://archive.org/download/Windows7ProfessionalRTMx64Dutch/de_windows_7_professional_x64_dvd_X15-65813.iso";
+}
+//Windows Vista links ------------------------------------------------------
+function Windows_vista_32_english() {
+window.location.href = "https://archive.org/download/en_windows_vista_enterprise_sp2_x86_dvd_342329_202010/en_windows_vista_enterprise_sp2_x86_dvd_342329.iso";
+}
+function Windows_vista_64_english() {
+window.location.href = "https://archive.org/download/2007-01-3708.0-windows-vista-x64-english/MSDN-Green-2007-01-3708.iso";
+}
+function Windows_vista_32_french() {
+window.location.href = "https://archive.org/download/WinVistaEnterpriseSP2FRA/fr_windows_vista_enterprise_sp2_x86_dvd_342352.iso";
+}
+function Windows_vista_64_french() {
+window.location.href = "https://archive.org/download/WinVistaEnterpriseSP2FRA/fr_windows_vista_enterprise_sp2_x64_dvd_342355.iso";
+}
+function Windows_vista_32_Dutch() {
+window.location.href = "https://archive.org/download/WindowsVistaBusinessSP1x86Dutch/Windows_Vista_Business_inkl_SP1-DE-x86-X14-56034.iso";
+}
+function Windows_vista_64_Dutch() {
+window.location.href = "https://archive.org/download/WindowsVistaBusinessSP1x64Dutch/Windows_Vista_Business_inkl_SP1-DE-x64-X14-56054.iso";
+}
+//Windows XP links ------------------------------------------------------
+function Windows_xp_32_english() {
+window.location.href = "https://archive.org/download/Windows_XP_Professional_SP3_x86.iso/Windows_XP_Professional_SP3_x86.iso";
+}
+function Windows_xp_64_english() {
+window.location.href = "https://archive.org/download/windows-xp-sp3-eng/WinXP%20Pro%20SP3.iso";
+}
+function Windows_xp_32_french() {
+window.location.href = "https://archive.org/download/WinXPProSP3French/fr_windows_xp_professional_with_service_pack_3_x86_cd_vl_x14-73982.iso";
+}
+function Windows_xp_64_french() {
+window.location.href = "https://archive.org/download/WinXPProx64EditionFRN/Windows.XP.Professional.x64.Edition.FR.iso";
+}
+function Windows_xp_32_Dutch() {
+window.location.href = "https://archive.org/download/WinXPHomeEditionSP3DEU/de_windows_xp_home_with_service_pack_3_x86_cd_x14-92416.iso";
+}
+//Windows 2000 links ------------------------------------------------------
+function Windows_2000_32_english() {
+window.location.href = "https://archive.org/download/windows-2000_en/Windows%202000.iso";
+}
+function Windows_2000_32_french() {
+window.location.href = "https://archive.org/download/MS_Windows_2000_French_MSDN_Pro-Server-AdvServer/W2KSEL_FR.iso";
+}
+function Windows_2000_32_Dutch() {
+window.location.href = "https://archive.org/download/Win2000Build2128DE/Windows%202000%20BETA%20Build%202128%20Professional%20Dutch.iso";
+}
+//Windows 98 links ------------------------------------------------------
+function Windows_98_32_english() {
+window.location.href = "https://archive.org/download/media-disc01_202106/Disc01.iso";
+}
+function Windows_98_32_french() {
+window.location.href = "https://archive.org/download/Windows98FirstEditionFrench/WINDOWS%2098%20FE%20OEM%20FR%20%28ImgBurn%29.iso";
+}
+function Windows_98_32_Dutch() {
+window.location.href = "https://archive.org/download/windows98_deutsch/Windows%2098.iso";
+}
+//Windows 95 links ------------------------------------------------------
+function Windows_95_32_english() {
+window.location.href = "https://archive.org/download/windows-95/WINDOWS95.ISO";
+}
+function Windows_95_32_french() {
+window.location.href = "https://archive.org/download/windows95francais/Windows95_Francais.iso";
+}
+function Windows_95_32_Dutch() {
+window.location.href = "https://archive.org/download/mswin95sammlung/MS_Win95_SR0_0795_Ger.iso";
+}
+
+//Windows 3.1 links ------------------------------------------------------
+function Windows_3_1_32_english() {
+window.location.href = "https://archive.org/download/en_winnt_3.1_1xx_oct1991_devkit_x86_en-US/en_winnt_3.1_1xx_oct1991_devkit_x86_en-US.iso";
+}
+function Windows_3_1_32_french() {
+window.location.href = "https://drive.google.com/uc?id=1S0RMh3PzDJG5S8aNWg2OPhAr-w3OKVyh";
+}
+function Windows_3_1_32_Dutch() {
+window.location.href = "https://drive.google.com/uc?id=1OWaCUCzSR9oudMvP0BWinGpqzPvNVnTc";
+}
+
+//Windows 2.1 links ------------------------------------------------------
+function Windows_2_1_32_english() {
+window.location.href = "https://archive.org/download/windows286-2.1/Windows286%202.1.7z";
+}
+function Windows_2_1_32_french() {
+window.location.href = "https://drive.google.com/uc?id=1DfbooU2U3NB4vWFGFQwkscIffqR0klka";
+}
+function Windows_2_1_32_Dutch() {
+window.location.href = "https://drive.google.com/uc?id=1nIAalyvN02cuFRrxTlDFb32ycIZwHKnI";
+}
+
+//Windows 1.1 links ------------------------------------------------------
+function Windows_1_1_32_english() {
+window.location.href = "https://drive.google.com/uc?id=1B8wKQI21qTdzm1wsYAwaSlfLa08J38rm";
+}
+function Windows_1_1_32_french() {
+window.location.href = "https://drive.google.com/uc?id=1upQhpBO9PgiH3EWCVhEHlscFDtk27Kgc";
+}
+function Windows_1_1_32_Dutch() {
+window.location.href = "https://drive.google.com/uc?id=1Zy-_acHSzZl9n25MetAT3FEF_rwi3jhh";
+}
